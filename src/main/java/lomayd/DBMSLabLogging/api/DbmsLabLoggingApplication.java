e0e8e02113d73e80682499455fd354603d4cfb90
lomayd <jinho10167@gmail.com>
2023-06-03 15:26:41 +0000

    feat: Project Initialize

---
+++ src/main/java/lomayd/DBMSLabLogging/api/DbmsLabLoggingApplication.java
@@ -0,0 +1,13 @@
+package lomayd.DBMSLabLogging.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbmsLabLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbmsLabLoggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lomayd/DBMSLabLogging/DbmsLabLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package lomayd.DBMSLabLogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbmsLabLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

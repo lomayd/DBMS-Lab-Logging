a757c70975bb1deb246859528f2a46a52b5f0f59
lomayd <jinho10167@gmail.com>
2023-06-14 08:03:06 +0000

    feat: ElasticSearch + LogStash + Kibana

--- src/main/java/lomayd/DBMSLabLogging/api/DbmsLabLoggingApplication.java
+++ src/main/java/lomayd/DBMSLabLogging/api/DbmsLabLoggingApplication.java
@@ -1,13 +1,26 @@
 package lomayd.DBMSLabLogging.api;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
-public class DbmsLabLoggingApplication {
+@Slf4j
+public class DbmsLabLoggingApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DbmsLabLoggingApplication.class, args);
 	}
-
+	
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		int i=0;
+		while(true){
+			Thread.sleep(1000);
+			log.info("lomayd-logging-elk-{}", ++i);
+		}
+	}
 }
